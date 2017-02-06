2ff3472b765853af6375649525a9714ff8c7dc1e
multiseven777 <porike90@gmail.com>
2017-02-06 15:03:18 +0100

    szimpla listázás

---
+++ src/StringArray.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StringArray {
+	
+	private static Scanner in;
+
+	public static void main(String[] args) {
+		
+		String answer;
+		do {
+		
+		System.out.println("Welcome to the Unbeleivable String ArrayList Simulator 2017!");
+		System.out.println("Please enter one of these magical words:");
+		System.out.println("'List', to view the current roster!");
+		System.out.println("'Add', to add a new unimaginable object!");
+		System.out.println("'Remove', to remove an object, duh!");
+		System.out.println("And don't forget, it's CaSe SeNsItIvE!");
+		
+		ArrayList<String> obj = new ArrayList<String>();
+		
+		obj.add("kacsa");
+		obj.add("kutya");
+		
+		in = new Scanner(System.in);
+		String n = in.next();
+		
+		switch(n) {
+		case "List":
+			System.out.println("Current list of fluffies: " + obj);
+			break;
+		case "Add":
+			System.out.println("Enter a happy new animal: ");
+			in = new Scanner(System.in);
+			String b = in.next();
+			if (obj.contains(b)) {
+				System.out.println("Already in the list silly!");
+				obj.remove(b);
+			} else {
+				obj.add(b);
+				System.out.println("Current list of fluffies after add: " + obj); }
+			break;
+		case "Remove":
+			System.out.println("Current list of fluffies: " + obj);
+			System.out.println("Choose which sad animal to remove: ");
+			in = new Scanner(System.in);
+			String c = in.next();
+			if (!obj.contains(c)) {
+				System.out.println(c + " is not in the list :(");
+			} else {
+				obj.remove(c);
+				System.out.println("Current list of fluffies after remove: " + obj); }
+			break;
+			
+		}
+		
+		System.out.println("Continue this magnificent game? Yes or No is enough to answer!");
+		in = new Scanner(System.in);
+		answer = in.next();
+		
+	} while (answer.equals("Yes"));
+		
+	}
+
+}
